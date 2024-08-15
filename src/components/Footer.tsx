f295f8d12f0886a14c4b3424df55e9adc3628c71
Lee de Verteuil <deverti@protonmail.com>
2024-08-15 14:54:19 -0400

    footer and right panel sizing change

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const Footer = () => {
   return (
-    <footer className="fixed bottom-0 p-5 w-full border-t border-colors z-[3] dark:bg-zinc-950 bg-white">
+    <footer className="fixed bottom-0 p-5 border-t border-colors z-[3] dark:bg-zinc-950 bg-white w-[calc(100%-27rem)]">
       <Tabs defaultValue="sheets">
         <TabsList>
           <TabsTrigger value="sheets">Sheets</TabsTrigger>
